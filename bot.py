11f106e98e62ba073d41b34b3278c4c4023ac3b6
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-06-05 17:56:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from os import getenv
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
-PICS = (getenv('PICS', '')).split()
+#PICS = (getenv('PICS', '')).split()
 
 mxabot = Client('Mxa_Movies_Bot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
