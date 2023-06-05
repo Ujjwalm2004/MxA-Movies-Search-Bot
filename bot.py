f97e482a58957d49c0e97b692a6c24835f2b8d77
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-06-05 19:08:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from os import getenv
 from bs4 import BeautifulSoup
 import requests
 import re
+from playwright.sync_api import Playwright, sync_playwright
 
 
 API_ID = int(getenv("API_ID"))
@@ -53,51 +54,70 @@ def search(client, message):
 @mxabot.on_message(filters.command("mkv"))
 def mkv_command(client, message):
     try:
+        # Get the link from the message text
         link = message.text.split(" ")[1]
+        # Check if the link contains "mkvcinemas"
         if "mkvcinemas" not in link:
             message.reply_text("Invalid link. Link must be of 'mkvcinemas.com'.")
             return
+        # Send processing message
         process_message = message.reply_text("Processing link, please wait...", quote=True)
         with sync_playwright() as playwright:
             final_link = process_link(playwright, link, message)
+        # Edit the process message with the final link
         process_message.edit_text(f"Link processed successfully! \n{final_link}", disable_web_page_preview=True)
     except IndexError:
+        # When no link is provided in the message
         message.reply_text("Please provide a valid link after the command. For example, `/mkv https://example.com`", quote=True)
     except Exception as e:
+        # When an error occurs during the processing of the link
         message.reply_text(f"An error occurred while processing the link: {e}", quote=True)
 
 
+# Define the mkv_command function
 @mxabot.on_message(filters.command("mkvc"))
 def mkvcinemas(client, message):
     try:
+        # Get the link from the message text
         link = message.text.split(" ")[1]
+        # Check if the link contains "mkvcinemas"
         if "mkvcinemas" not in link:
             message.reply_text("Invalid link. Link must be of 'mkvcinemas.com'.")
             return
+        # Send processing message
         process_message = message.reply_text("Processing link, please wait...", quote=True)
+        # Use the requests library to get the HTML content of the link
         response = requests.get(link)
         html_content = response.text
+
+        # Use BeautifulSoup to parse the HTML content and extract all links from the page
         soup = BeautifulSoup(html_content, "html.parser")
         links = [a["href"] for a in soup.find_all("a", {"class": "gdlink"}, href=True)]
 
+        # Create an empty list to store the final links
         final_links = []
-
+        # Loop through the links and process only those that contain "mkvcinemas" in the URL
         for link in links:
             if "mkvcinemas" in link:
                 with sync_playwright() as playwright:
                     final_link = process_link(playwright, link, message)
+                    # Extract the title of the link and append it to the final link
                     title = soup.find("a", {"href": link, "class": "gdlink"}).text
                     final_link += f" - {title}\n"
                     final_links.append(final_link)
+        # Edit the process message with the final links
         final_links_text = "\n".join(final_links)
+        # Split the final links into chunks of up to 10 links per message
         links_per_message = 5
         final_links_chunks = [final_links[i:i+links_per_message] for i in range(0, len(final_links), links_per_message)]
         for i, links_chunk in enumerate(final_links_chunks):
+            # Send each chunk of links as a separate message
             message_text = f"Links processed successfully! (Part {i+1}/{len(final_links_chunks)}) \n\n" + "\n".join(links_chunk)
             message.reply_text(message_text, disable_web_page_preview=True)
-    except IndexError:
-        message.reply_text("Please provide a valid link after the command. For example, `/mkv https://example.com`", quote=True)
 
+    except IndexError:
+        # When no link is provided in the message
+        message.reply_text("Please provide a valid link after the command. For example, `/mkv https://example.com`", quote=True)
     except Exception as e:
         # When an error occurs during the processing of the link
         message.reply_text(f"An error occurred while processing the link: {e}", quote=True)
