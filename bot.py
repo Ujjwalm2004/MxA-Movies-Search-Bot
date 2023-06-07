5eb291c2e2d929c3a9647483796147b5bd0b8712
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-06-07 21:59:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,6 @@ import time
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
-#PICS = (getenv('PICS', '')).split()
 
 mxabot = Client('Mxa_Movies_Bot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 FSUB_CHANNEL = "Movies_X_Animes"
